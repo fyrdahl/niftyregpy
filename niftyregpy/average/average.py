5ce1a21345010e268fb186200866545448299397
Alexander Fyrdahl <alexander.fyrdahl@gmail.com>
2022-02-04 13:55:30 -0500

    Implement the remaining parts of reg_average, including simple tests

--- niftyregpy/average/average.py
+++ niftyregpy/average/average.py
@@ -60,13 +60,13 @@ def _avg_nii(input, verbose=False):
             return None
 
 
-def avg_lts(input, verbose=False):
+def avg_lts(aff, verbose=False):
 
     """
-    It will estimate the robust average affine matrix by considering outliers.
+    Estimate the robust average affine matrix by considering outliers.
     """
 
-    assert all([a.shape == (4, 4) for a in input]), "Not affine matrices"
+    assert all([a.shape == (4, 4) for a in aff]), "Not affine matrices"
 
     with tmp.TemporaryDirectory() as tmp_folder:
 
@@ -74,7 +74,7 @@ def avg_lts(input, verbose=False):
         cmd_str = "reg_average "
         cmd_str += f"{tmp_folder}output.txt  -avg "
 
-        for i, x in enumerate(input):
+        for i, x in enumerate(aff):
             write_txt(f"{tmp_folder}avg_{i}.txt", x)
             cmd_str += f"{tmp_folder}avg_{i}.txt "
 
@@ -87,17 +87,122 @@ def avg_lts(input, verbose=False):
             return None
 
 
-def avg_tran():
-    raise NotImplementedError
+def avg_tran(ref, tran, flo, verbose=False):
+
+    """
+    All input images are resampled into the space of <reference image> and
+    averaged A cubic spline interpolation scheme is used for resampling.
+    """
+
+    assert len(flo) == len(
+        tran
+    ), "Non-matching number of floating images and transforms"
+
+    with tmp.TemporaryDirectory() as tmp_folder:
+
+        tmp_folder += path.sep
+        cmd_str = "reg_average "
+        cmd_str += f"{tmp_folder}output.nii  -avg_tran "
+
+        write_nifti(f"{tmp_folder}ref.nii", ref)
+        cmd_str += f"{tmp_folder}ref.nii "
+
+        for i, x in enumerate(zip(tran, flo)):
+            write_nifti(f"{tmp_folder}avg_tran_{i}.nii", x[0])
+            write_nifti(f"{tmp_folder}avg_flo_{i}.nii", x[1])
+            cmd_str += f"{tmp_folder}avg_tran_{i}.nii {tmp_folder}avg_flo_{i}.nii "
+
+        if verbose:
+            print(cmd_str)
+
+        if call_niftyreg(cmd_str, verbose):
+            return read_nifti(f"{tmp_folder}output.nii")
+        else:
+            return None
 
 
-def demean1():
-    raise NotImplementedError
+def demean1(ref, aff, flo, verbose=False):
+
+    """
+    The demean1 option enforces the mean of all affine matrices to have a
+    Jacobian determinant equal to one. This is done by computing the average
+    transformation by considering only the scaling and shearing arguments.
+    The inverse of this computed average matrix is then removed to all input
+    affine matrix before resampling all floating images to the user-defined
+    reference space
+    """
+
+    assert len(aff) == len(flo), "Non-matching number of floating images and transforms"
+
+    with tmp.TemporaryDirectory() as tmp_folder:
+
+        tmp_folder += path.sep
+        cmd_str = "reg_average "
+        cmd_str += f"{tmp_folder}output.nii -demean1 "
+
+        write_nifti(f"{tmp_folder}ref.nii", ref)
+        cmd_str += f"{tmp_folder}ref.nii "
+
+        for i, x in enumerate(zip(aff, flo)):
+            write_txt(f"{tmp_folder}avg_aff_{i}.txt", x[0])
+            write_nifti(f"{tmp_folder}avg_flo_{i}.nii", x[1])
+            cmd_str += f"{tmp_folder}avg_aff_{i}.nii {tmp_folder}avg_flo_{i}.nii "
+
+        if verbose:
+            print(cmd_str)
+
+        if call_niftyreg(cmd_str, verbose):
+            return read_nifti(f"{tmp_folder}output.nii")
+        else:
+            return None
 
 
-def demean2():
-    raise NotImplementedError
+def demean2(ref, tran, flo, verbose=False):
+
+    with tmp.TemporaryDirectory() as tmp_folder:
+
+        tmp_folder += path.sep
+        cmd_str = "reg_average "
+        cmd_str += f"{tmp_folder}output.nii -demean2 "
+
+        write_nifti(f"{tmp_folder}ref.nii", ref)
+        cmd_str += f"{tmp_folder}ref.nii "
+
+        for i, x in enumerate(zip(tran, flo)):
+            write_nifti(f"{tmp_folder}avg_tran_{i}.nii", x[0])
+            write_nifti(f"{tmp_folder}avg_flo_{i}.nii", x[1])
+            cmd_str += f"{tmp_folder}avg_tran_{i}.nii {tmp_folder}avg_flo_{i}.nii "
+
+        if verbose:
+            print(cmd_str)
+
+        if call_niftyreg(cmd_str, verbose):
+            return read_nifti(f"{tmp_folder}output.nii")
+        else:
+            return None
 
 
-def demean3():
-    raise NotImplementedError
+def demean3(ref, aff, tran, flo, verbose=False):
+
+    with tmp.TemporaryDirectory() as tmp_folder:
+
+        tmp_folder += path.sep
+        cmd_str = "reg_average "
+        cmd_str += f"{tmp_folder}output.nii -demean3 "
+
+        write_nifti(f"{tmp_folder}ref.nii", ref)
+        cmd_str += f"{tmp_folder}ref.nii "
+
+        for i, x in enumerate(zip(aff, tran, flo)):
+            write_txt(f"{tmp_folder}avg_aff_{i}.txt", x[0])
+            write_nifti(f"{tmp_folder}avg_tran_{i}.nii", x[1])
+            write_nifti(f"{tmp_folder}avg_flo_{i}.nii", x[2])
+            cmd_str += f"{tmp_folder}avg_aff_{i}.txt  {tmp_folder}avg_tran_{i}.nii {tmp_folder}avg_flo_{i}.nii "
+
+        if verbose:
+            print(cmd_str)
+
+        if call_niftyreg(cmd_str, verbose):
+            return read_nifti(f"{tmp_folder}output.nii")
+        else:
+            return None
--- tests/test_average.py
+++ tests/test_average.py
@@ -23,3 +23,49 @@ def test_avg_lts():
     aff2 = common.create_random_affine()
     output = average.avg_lts((aff1, aff2))
     assert output is not None
+
+
+def test_avg_tran():
+    length = 256
+    ref = common.create_random_array((length, length))
+    flo1 = common.create_random_array((length, length))
+    flo2 = common.create_random_array((length, length))
+    tran1 = common.create_random_array((length, length))
+    tran2 = common.create_random_array((length, length))
+    output = average.avg_tran(ref, (flo1, flo2), (tran1, tran2))
+    assert output is not None
+
+
+def demean1():
+    length = 256
+    ref = common.create_random_array((length, length))
+    aff1 = common.create_random_affine()
+    aff2 = common.create_random_affine()
+    flo1 = common.create_random_array((length, length))
+    flo2 = common.create_random_array((length, length))
+    output = average.avg_tran(ref, (aff1, aff2), (flo1, flo2))
+    assert output is not None
+
+
+def demean2():
+    length = 256
+    ref = common.create_random_array((length, length))
+    tran1 = common.create_random_array((length, length))
+    tran2 = common.create_random_array((length, length))
+    flo1 = common.create_random_array((length, length))
+    flo2 = common.create_random_array((length, length))
+    output = average.avg_tran(ref, (tran1, tran2), (flo1, flo2))
+    assert output is not None
+
+
+def demean3():
+    length = 256
+    ref = common.create_random_array((length, length))
+    aff1 = common.create_random_affine()
+    aff2 = common.create_random_affine()
+    flo1 = common.create_random_array((length, length))
+    flo2 = common.create_random_array((length, length))
+    tran1 = common.create_random_array((length, length))
+    tran2 = common.create_random_array((length, length))
+    output = average.avg_tran(ref, (aff1, aff2), (flo1, flo2), (tran1, tran2))
+    assert output is not None
