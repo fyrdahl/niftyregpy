a0f986dd6fbbb91f8441456504819018ec1ab26f
Alexander Fyrdahl <alexander.fyrdahl@gmail.com>
2022-02-11 16:10:47 -0500

    Update code style in tests suite

--- tests/test_average.py
+++ tests/test_average.py
@@ -3,69 +3,64 @@ import test_common as common
 from niftyregpy import average
 
 
-def test_avg_affine():
-    aff1 = common.create_random_affine()
-    aff2 = common.create_random_affine()
-    output = average.avg((aff1, aff2))
-    assert output is not None
+class TestAverage():
 
+    def setup_method(self, method):
+        self.length = 256
+        common.seed_random_generators()
 
-def test_avg_image():
-    length = 256
-    img1 = common.create_random_array((length, length))
-    img2 = common.create_random_array((length, length))
-    output = average.avg((img1, img2))
-    assert output is not None
+    def test_avg_affine(self):
+        aff1 = common.create_random_affine()
+        aff2 = common.create_random_affine()
+        output = average.avg((aff1, aff2))
+        assert output is not None
 
+    def test_avg_image(self):
+        img1 = common.create_random_array((self.length, self.length))
+        img2 = common.create_random_array((self.length, self.length))
+        output = average.avg((img1, img2))
+        assert output is not None
 
-def test_avg_lts():
-    aff1 = common.create_random_affine()
-    aff2 = common.create_random_affine()
-    output = average.avg_lts((aff1, aff2))
-    assert output is not None
+    def test_avg_lts(self):
+        aff1 = common.create_random_affine()
+        aff2 = common.create_random_affine()
+        output = average.avg_lts((aff1, aff2))
+        assert output is not None
 
+    def test_avg_tran(self):
+        ref = common.create_random_array((self.length, self.length))
+        flo1 = common.create_random_array((self.length, self.length))
+        flo2 = common.create_random_array((self.length, self.length))
+        tran1 = common.create_random_array((self.length, self.length))
+        tran2 = common.create_random_array((self.length, self.length))
+        output = average.avg_tran(ref, (flo1, flo2), (tran1, tran2))
+        assert output is not None
 
-def test_avg_tran():
-    length = 256
-    ref = common.create_random_array((length, length))
-    flo1 = common.create_random_array((length, length))
-    flo2 = common.create_random_array((length, length))
-    tran1 = common.create_random_array((length, length))
-    tran2 = common.create_random_array((length, length))
-    output = average.avg_tran(ref, (flo1, flo2), (tran1, tran2))
-    assert output is not None
+    def demean1(self):
+        ref = common.create_random_array((self.length, self.length))
+        aff1 = common.create_random_affine()
+        aff2 = common.create_random_affine()
+        flo1 = common.create_random_array((self.length, self.length))
+        flo2 = common.create_random_array((self.length, self.length))
+        output = average.avg_tran(ref, (aff1, aff2), (flo1, flo2))
+        assert output is not None
 
+    def demean2(self):
+        ref = common.create_random_array((self.length, self.length))
+        tran1 = common.create_random_array((self.length, self.length))
+        tran2 = common.create_random_array((self.length, self.length))
+        flo1 = common.create_random_array((self.length, self.length))
+        flo2 = common.create_random_array((self.length, self.length))
+        output = average.avg_tran(ref, (tran1, tran2), (flo1, flo2))
+        assert output is not None
 
-def demean1():
-    length = 256
-    ref = common.create_random_array((length, length))
-    aff1 = common.create_random_affine()
-    aff2 = common.create_random_affine()
-    flo1 = common.create_random_array((length, length))
-    flo2 = common.create_random_array((length, length))
-    output = average.avg_tran(ref, (aff1, aff2), (flo1, flo2))
-    assert output is not None
-
-
-def demean2():
-    length = 256
-    ref = common.create_random_array((length, length))
-    tran1 = common.create_random_array((length, length))
-    tran2 = common.create_random_array((length, length))
-    flo1 = common.create_random_array((length, length))
-    flo2 = common.create_random_array((length, length))
-    output = average.avg_tran(ref, (tran1, tran2), (flo1, flo2))
-    assert output is not None
-
-
-def demean3():
-    length = 256
-    ref = common.create_random_array((length, length))
-    aff1 = common.create_random_affine()
-    aff2 = common.create_random_affine()
-    flo1 = common.create_random_array((length, length))
-    flo2 = common.create_random_array((length, length))
-    tran1 = common.create_random_array((length, length))
-    tran2 = common.create_random_array((length, length))
-    output = average.avg_tran(ref, (aff1, aff2), (flo1, flo2), (tran1, tran2))
-    assert output is not None
+    def demean3(self):
+        ref = common.create_random_array((self.length, self.length))
+        aff1 = common.create_random_affine()
+        aff2 = common.create_random_affine()
+        flo1 = common.create_random_array((self.length, self.length))
+        flo2 = common.create_random_array((self.length, self.length))
+        tran1 = common.create_random_array((self.length, self.length))
+        tran2 = common.create_random_array((self.length, self.length))
+        output = average.avg_tran(ref, (aff1, aff2), (flo1, flo2), (tran1, tran2))
+        assert output is not None
--- tests/test_reg.py
+++ tests/test_reg.py
@@ -3,73 +3,67 @@ import test_common as common
 
 from niftyregpy import reg
 
+class TestReg():
 
-def test_aladin_default():
-    length = 256
-    size = common.random_float(length / 8, length * 7 / 8)
-    ref = common.create_rect(length, w=size, h=size)
-    flo = common.create_rect(length, w=size, h=size)
-    noisy_ref = common.add_noise(ref)
-    noisy_flo = common.add_noise(flo)
-    output = reg.aladin(noisy_ref, noisy_flo, verbose=False)
-    assert output is not None
+    def setup_method(self, method):
+        self.length = 256
+        common.seed_random_generators()
 
+    def test_aladin_default(self):
+        size = common.random_float(self.length / 8, self.length * 7 / 8)
+        ref = common.create_rect(self.length, w=size, h=size)
+        flo = common.create_rect(self.length, w=size, h=size)
+        noisy_ref = common.add_noise(ref)
+        noisy_flo = common.add_noise(flo)
+        output = reg.aladin(noisy_ref, noisy_flo, verbose=False)
+        assert output is not None
 
-def test_f3d_default():
-    length = 256
-    rad_ref = common.random_float(length / 8, length * 7 / 8)
-    ref = common.create_circle(length, r=rad_ref)
-    noisy_ref = common.add_noise(ref)
-    rad_flo = common.random_float(length / 8, length * 7 / 8)
-    flo = common.create_circle(length, r=rad_flo)
-    noisy_flo = common.add_noise(flo)
-    output = reg.f3d(noisy_ref, noisy_flo, pad=0, verbose=False)
-    assert output is not None
+    def test_f3d_default(self):
+        rad_ref = common.random_float(self.length / 8, self.length * 7 / 8)
+        ref = common.create_circle(self.length, r=rad_ref)
+        noisy_ref = common.add_noise(ref)
+        rad_flo = common.random_float(self.length / 8, self.length * 7 / 8)
+        flo = common.create_circle(self.length, r=rad_flo)
+        noisy_flo = common.add_noise(flo)
+        output = reg.f3d(noisy_ref, noisy_flo, pad=0, verbose=False)
+        assert output is not None
 
+    def test_reg_float(self):
+        input = common.create_random_array((256, 256), np.float32)
+        output = reg.tools(input, float=True)
+        assert output is not None
 
-def test_reg_float():
-    input = common.create_random_array((256, 256), np.float32)
-    output = reg.tools(input, float=True)
-    assert output is not None
+    def test_reg_down(self):
+        input = common.create_random_array((256, 256), np.float32)
+        output = reg.tools(input, down=True)
+        assert output.shape == tuple(x // 2 for x in input.shape)
 
+    def test_reg_smoS_scalar_input(self):
+        input = common.create_random_array((256, 256), np.float32)
+        output = reg.tools(input, smoS=common.random_float())
+        assert output is not None
 
-def test_reg_down():
-    input = common.create_random_array((256, 256), np.float32)
-    output = reg.tools(input, down=True)
-    assert output.shape == tuple(x // 2 for x in input.shape)
+    def test_reg_smoS_tuple_input(self):
+        input = common.create_random_array((256, 256), np.float32)
+        output = reg.tools(input, smoS=common.create_random_tuple(3))
+        assert output is not None
 
+    def test_reg_smoG_scalar_input(self):
+        input = common.create_random_array((256, 256), np.float32)
+        output = reg.tools(input, smoG=common.random_float())
+        assert output is not None
 
-def test_reg_smoS_scalar_input():
-    input = common.create_random_array((256, 256), np.float32)
-    output = reg.tools(input, smoS=common.random_float())
-    assert output is not None
+    def test_reg_smoG_tuple_input(self):
+        input = common.create_random_array((256, 256), np.float32)
+        output = reg.tools(input, smoG=common.create_random_tuple(3))
+        assert output is not None
 
+    def test_reg_smoL_scalar_input(self):
+        input = common.create_random_array((256, 256), np.float32)
+        output = reg.tools(input, smoL=common.random_float())
+        assert output is not None
 
-def test_reg_smoS_tuple_input():
-    input = common.create_random_array((256, 256), np.float32)
-    output = reg.tools(input, smoS=common.create_random_tuple(3))
-    assert output is not None
-
-
-def test_reg_smoG_scalar_input():
-    input = common.create_random_array((256, 256), np.float32)
-    output = reg.tools(input, smoG=common.random_float())
-    assert output is not None
-
-
-def test_reg_smoG_tuple_input():
-    input = common.create_random_array((256, 256), np.float32)
-    output = reg.tools(input, smoG=common.create_random_tuple(3))
-    assert output is not None
-
-
-def test_reg_smoL_scalar_input():
-    input = common.create_random_array((256, 256), np.float32)
-    output = reg.tools(input, smoL=common.random_float())
-    assert output is not None
-
-
-def test_reg_smoL_tuple_input():
-    input = common.create_random_array((256, 256), np.float32)
-    output = reg.tools(input, smoL=common.create_random_tuple(3))
-    assert output is not None
+    def test_reg_smoL_tuple_input(self):
+        input = common.create_random_array((256, 256), np.float32)
+        output = reg.tools(input, smoL=common.create_random_tuple(3))
+        assert output is not None
