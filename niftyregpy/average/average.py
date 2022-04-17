21856bbf22a35e44b2bb5397c0df29bf5ed69264
Alexander Fyrdahl <alexander.fyrdahl@gmail.com>
2022-04-17 17:34:48 -0400

    Fixed broken average

--- niftyregpy/average/average.py
+++ niftyregpy/average/average.py
@@ -1,10 +1,10 @@
+import os
 import tempfile as tmp
-from os import path
 
 from ..utils import call_niftyreg, read_nifti, read_txt, write_nifti, write_txt
 
 
-def avg(input):
+def avg(input, verbose=False):
 
     """
     If the input are images, the intensities are averaged
@@ -13,28 +13,27 @@ def avg(input):
     """
 
     if all([a.shape == (4, 4) for a in input]):
-        return _avg_txt(input)
+        return _avg_txt(input, verbose)
     else:
-        return _avg_nii(input)
+        return _avg_nii(input, verbose)
 
 
 def _avg_txt(input, verbose=False):
 
     with tmp.TemporaryDirectory() as tmp_folder:
 
-        tmp_folder += path.sep
         cmd_str = "reg_average "
-        cmd_str += f"{tmp_folder}output.txt  -avg "
+        cmd_str += os.path.join(tmp_folder, "output.txt") + "  -avg "
 
         for i, x in enumerate(input):
-            write_txt(f"{tmp_folder}avg_{i}.txt", x)
-            cmd_str += f"{tmp_folder}avg_{i}.txt "
+            write_txt(os.path.join(tmp_folder, f"avg_{i}.txt"), x)
+            cmd_str += os.path.join(tmp_folder, f"avg_{i}.txt") + " "
 
         if verbose:
             print(cmd_str)
 
         if call_niftyreg(cmd_str, verbose):
-            return read_txt(f"{tmp_folder}output.txt")
+            return read_txt(os.path.join(tmp_folder, "output.txt"))
         else:
             return None
 
@@ -43,19 +42,18 @@ def _avg_nii(input, verbose=False):
 
     with tmp.TemporaryDirectory() as tmp_folder:
 
-        tmp_folder += path.sep
         cmd_str = "reg_average "
-        cmd_str += f"{tmp_folder}output.nii  -avg "
+        cmd_str += os.path.join(tmp_folder, "output.nii") + "  -avg "
 
         for i, x in enumerate(input):
-            write_nifti(f"{tmp_folder}avg_{i}.nii", x)
-            cmd_str += f"{tmp_folder}avg_{i}.nii "
+            write_nifti(os.path.join(tmp_folder, f"avg_{i}.nii"), x)
+            cmd_str += os.path.join(tmp_folder, f"avg_{i}.nii") + " "
 
         if verbose:
             print(cmd_str)
 
         if call_niftyreg(cmd_str, verbose):
-            return read_nifti(f"{tmp_folder}output.nii")
+            return read_nifti(os.path.join(tmp_folder, "output.nii"))
         else:
             return None
 
@@ -63,26 +61,26 @@ def _avg_nii(input, verbose=False):
 def avg_lts(aff, verbose=False):
 
     """
-    Estimate the robust average affine matrix by considering outliers.
+    Estimate the robust average affine matrix by considering half of the
+    matrices as outliers.
     """
 
     assert all([a.shape == (4, 4) for a in aff]), "Not affine matrices"
 
     with tmp.TemporaryDirectory() as tmp_folder:
 
-        tmp_folder += path.sep
         cmd_str = "reg_average "
-        cmd_str += f"{tmp_folder}output.txt  -avg "
+        cmd_str += os.path.join(tmp_folder, "output.txt") + "  -avg "
 
         for i, x in enumerate(aff):
-            write_txt(f"{tmp_folder}avg_{i}.txt", x)
-            cmd_str += f"{tmp_folder}avg_{i}.txt "
+            write_txt(os.path.join(tmp_folder, f"avg_{i}.txt"), x)
+            cmd_str += os.path.join(tmp_folder, f"avg_{i}.txt") + " "
 
         if verbose:
             print(cmd_str)
 
         if call_niftyreg(cmd_str, verbose):
-            return read_txt(f"{tmp_folder}output.txt")
+            return read_txt(os.path.join(tmp_folder, "output.txt"))
         else:
             return None
 
@@ -100,23 +98,27 @@ def avg_tran(ref, tran, flo, verbose=False):
 
     with tmp.TemporaryDirectory() as tmp_folder:
 
-        tmp_folder += path.sep
         cmd_str = "reg_average "
-        cmd_str += f"{tmp_folder}output.nii  -avg_tran "
+        cmd_str += os.path.join(tmp_folder, "output.nii") + " -avg_tran "
 
-        write_nifti(f"{tmp_folder}ref.nii", ref)
-        cmd_str += f"{tmp_folder}ref.nii "
+        write_nifti(os.path.join(tmp_folder, "ref.nii"), ref)
+        cmd_str += os.path.join(tmp_folder, "ref.nii") + " "
 
         for i, x in enumerate(zip(tran, flo)):
-            write_nifti(f"{tmp_folder}avg_tran_{i}.nii", x[0])
-            write_nifti(f"{tmp_folder}avg_flo_{i}.nii", x[1])
-            cmd_str += f"{tmp_folder}avg_tran_{i}.nii {tmp_folder}avg_flo_{i}.nii "
+            write_nifti(os.path.join(tmp_folder, f"avg_tran_{i}.nii"), x[0])
+            write_nifti(os.path.join(tmp_folder, f"avg_flo_{i}.nii"), x[1])
+            cmd_str += (
+                os.path.join(tmp_folder, f"avg_tran_{i}.nii")
+                + " "
+                + os.path.join(tmp_folder, f"avg_flo_{i}.nii")
+                + " "
+            )
 
         if verbose:
             print(cmd_str)
 
         if call_niftyreg(cmd_str, verbose):
-            return read_nifti(f"{tmp_folder}output.nii")
+            return read_nifti(os.path.join(tmp_folder, "output.nii"))
         else:
             return None
 
@@ -136,17 +138,21 @@ def demean1(ref, aff, flo, verbose=False):
 
     with tmp.TemporaryDirectory() as tmp_folder:
 
-        tmp_folder += path.sep
         cmd_str = "reg_average "
-        cmd_str += f"{tmp_folder}output.nii -demean1 "
+        cmd_str += os.path.join(tmp_folder, "output.nii") + "  -demean1 "
 
         write_nifti(f"{tmp_folder}ref.nii", ref)
         cmd_str += f"{tmp_folder}ref.nii "
 
         for i, x in enumerate(zip(aff, flo)):
-            write_txt(f"{tmp_folder}avg_aff_{i}.txt", x[0])
-            write_nifti(f"{tmp_folder}avg_flo_{i}.nii", x[1])
-            cmd_str += f"{tmp_folder}avg_aff_{i}.nii {tmp_folder}avg_flo_{i}.nii "
+            write_txt(os.path.join(tmp_folder, f"avg_aff_{i}.txt"), x[0])
+            write_nifti(os.path.join(tmp_folder, f"avg_flo_{i}.nii"), x[1])
+            cmd_str += (
+                os.path.join(tmp_folder, f"avg_aff_{i}.nii")
+                + " "
+                + os.path.join(tmp_folder, f"avg_flo_{i}.nii")
+                + " "
+            )
 
         if verbose:
             print(cmd_str)
@@ -161,17 +167,21 @@ def demean2(ref, tran, flo, verbose=False):
 
     with tmp.TemporaryDirectory() as tmp_folder:
 
-        tmp_folder += path.sep
         cmd_str = "reg_average "
         cmd_str += f"{tmp_folder}output.nii -demean2 "
 
-        write_nifti(f"{tmp_folder}ref.nii", ref)
+        write_nifti(os.path.join(tmp_folder, "ref.nii"), ref)
         cmd_str += f"{tmp_folder}ref.nii "
 
         for i, x in enumerate(zip(tran, flo)):
-            write_nifti(f"{tmp_folder}avg_tran_{i}.nii", x[0])
-            write_nifti(f"{tmp_folder}avg_flo_{i}.nii", x[1])
-            cmd_str += f"{tmp_folder}avg_tran_{i}.nii {tmp_folder}avg_flo_{i}.nii "
+            write_nifti(os.path.join(tmp_folder, f"avg_tran_{i}.nii"), x[0])
+            write_nifti(os.path.join(tmp_folder, f"avg_flo_{i}.nii"), x[1])
+            cmd_str += (
+                os.path.join(tmp_folder, f"avg_tran_{i}.nii")
+                + " "
+                + os.path.join(tmp_folder, f"avg_flo_{i}.nii")
+                + " "
+            )
 
         if verbose:
             print(cmd_str)
@@ -186,18 +196,24 @@ def demean3(ref, aff, tran, flo, verbose=False):
 
     with tmp.TemporaryDirectory() as tmp_folder:
 
-        tmp_folder += path.sep
         cmd_str = "reg_average "
-        cmd_str += f"{tmp_folder}output.nii -demean3 "
+        cmd_str += os.path.join(tmp_folder, "output.nii") + "-demean3 "
 
-        write_nifti(f"{tmp_folder}ref.nii", ref)
-        cmd_str += f"{tmp_folder}ref.nii "
+        write_nifti(os.path.join(tmp_folder, "ref.nii"), ref)
+        cmd_str += os.path.join(tmp_folder, "ref.nii") + " "
 
         for i, x in enumerate(zip(aff, tran, flo)):
-            write_txt(f"{tmp_folder}avg_aff_{i}.txt", x[0])
-            write_nifti(f"{tmp_folder}avg_tran_{i}.nii", x[1])
-            write_nifti(f"{tmp_folder}avg_flo_{i}.nii", x[2])
-            cmd_str += f"{tmp_folder}avg_aff_{i}.txt  {tmp_folder}avg_tran_{i}.nii {tmp_folder}avg_flo_{i}.nii "
+            write_txt(os.path.join(tmp_folder, f"avg_aff_{i}.txt"), x[0])
+            write_nifti(os.path.join(tmp_folder, f"avg_tran_{i}.nii"), x[1])
+            write_nifti(os.path.join(tmp_folder, f"avg_flo_{i}.nii"), x[2])
+            cmd_str += (
+                os.path.join(tmp_folder, f"avg_aff_{i}.txt")
+                + " "
+                + os.path.join(tmp_folder, f"avg_tran_{i}.nii")
+                + " "
+                + os.path.join(tmp_folder, f"avg_flo_{i}.nii")
+                + " "
+            )
 
         if verbose:
             print(cmd_str)
