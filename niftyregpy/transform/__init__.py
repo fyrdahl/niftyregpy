78616650607871cbb50bf843f1137cb4f18f0c62
Alexander Fyrdahl <alexander.fyrdahl@gmail.com>
2022-06-13 18:48:00 -0400

    Add files for transforms (not implemented)

---
+++ niftyregpy/transform/__init__.py
@@ -0,0 +1,13 @@
+from .transform import (
+    aff2rig,
+    comp,
+    deform,
+    disp,
+    flirtAff2NR,
+    flow,
+    half,
+    invAff,
+    invNrr,
+    makeAff,
+    updSform,
+)
---
+++ niftyregpy/transform/transform.py
@@ -0,0 +1,48 @@
+import tempfile as tmp
+from os import path
+
+import numpy as np
+
+
+def deform():
+    raise NotImplementedError
+
+
+def disp():
+    raise NotImplementedError
+
+
+def flow():
+    raise NotImplementedError
+
+
+def comp():
+    raise NotImplementedError
+
+
+def updSform():
+    raise NotImplementedError
+
+
+def invAff():
+    raise NotImplementedError
+
+
+def invNrr():
+    raise NotImplementedError
+
+
+def half():
+    raise NotImplementedError
+
+
+def makeAff():
+    raise NotImplementedError
+
+
+def aff2rig():
+    raise NotImplementedError
+
+
+def flirtAff2NR():
+    raise NotImplementedError
