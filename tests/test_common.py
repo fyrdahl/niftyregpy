526bacad9df74835a9c55fd549f1638670118135
Alexander Fyrdahl <alexander.fyrdahl@gmail.com>
2022-06-18 12:48:56 -0400

    Refactor some tests

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,6 +1,7 @@
 import random
 
 import numpy as np
+from skimage import transform
 
 
 def random_float(low=0.0, high=1.0):
@@ -82,45 +83,20 @@ def add_noise(array, mean=0, std=0.1):
     return array + np.random.normal(mean, std, array.shape)
 
 
-def rotate(array, phi=0.0):
-
-    if np.mod(phi, 2 * np.pi) == 0:
-        return array
-
-    height, width = array.shape
-
-    new_height = round(abs(height * np.cos(phi)) + abs(width * np.sin(phi))) + 1
-    new_width = round(abs(width * np.cos(phi)) + abs(height * np.sin(phi))) + 1
-
-    array_out = np.zeros((new_height, new_width))
-
-    original_centre_height = round(((height + 1) / 2) - 1)
-    original_centre_width = round(((width + 1) / 2) - 1)
-
-    new_centre_height = round(((new_height + 1) / 2) - 1)
-    new_centre_width = round(((new_width + 1) / 2) - 1)
-
-    for i in range(height):
-        for j in range(width):
-            y = height - 1 - i - original_centre_height
-            x = width - 1 - j - original_centre_width
-
-            tangent = np.tan(phi / 2)
-            new_x = round(x - y * tangent)
-            new_y = round(new_x * np.sin(phi) + y)
-            new_x = round(new_x - new_y * tangent)
-
-            new_y = new_centre_height - new_y
-            new_x = new_centre_width - new_x
-
-            array_out[new_y, new_x] = array[i, j]
-
-    return array_out
-
-
 def jaccard(array1, array2):
     array1 = np.asarray(array1).astype(bool)
     array2 = np.asarray(array2).astype(bool)
     intersection = np.logical_and(array1, array2)
     union = np.logical_or(array1, array2)
     return intersection.sum() / float(union.sum())
+
+
+def apply_swirl(array, c, s, r):
+    if c != tuple:
+        c = (c, c)
+    return transform.swirl(
+        array,
+        center=c,
+        strength=s,
+        radius=r,
+    )
--- tests/test_reg.py
+++ tests/test_reg.py
@@ -1,5 +1,5 @@
 import numpy as np
-from niftyregpy import reg, utils
+from niftyregpy import reg
 from skimage import transform
 
 import test_common as common
@@ -8,149 +8,128 @@ import test_common as common
 class TestReg:
     def setup_method(self, method):
         self.length = 256
-        self.tol = 0.1
+        self.tol = 0.2
         self.non_linearity = 10
+        self.verbose = True
+        self.size = common.random_float(self.length / 8, self.length * 7 / 8)
         common.seed_random_generators()
 
     def test_resample_affine(self):
-        size = common.random_float(self.length / 8, self.length * 7 / 8)
-        ref = common.create_rect(self.length, w=size, h=size)
-        flo = common.create_rect(self.length, w=size, h=size)
+        ref = common.create_rect(self.length, w=self.size, h=self.size)
+        flo = common.create_rect(self.length, w=self.size, h=self.size)
         affine = common.random_rigid()
-        output = reg.resample(ref, flo, trans=affine, inter=0, verbose=True)
+        output = reg.resample(ref, flo, trans=affine, inter=0, verbose=self.verbose)
         assert output is not None
 
     def test_aladin_default(self):
-        size = common.random_float(self.length / 8, self.length * 7 / 8)
-        ref = common.create_rect(self.length, w=size, h=size)
-        tform1 = transform.AffineTransform(translation=-self.length // 2)
-        tform2 = transform.AffineTransform(rotation=45, shear=10)
-        tform3 = transform.AffineTransform(translation=self.length // 4)
-        flo = transform.warp(ref, tform1 + tform2 + tform3)
-        output = reg.aladin(ref, flo, pad=0, verbose=True)
+        ref = common.create_rect(self.length, w=self.size, h=self.size)
+        tform_trans = transform.AffineTransform(translation=-self.length // 2)
+        tform_rot = transform.AffineTransform(rotation=45, shear=10)
+        flo = transform.warp(ref, tform_trans + tform_rot + tform_trans.inverse)
+        output = reg.aladin(ref, flo, pad=0, verbose=self.verbose)
         assert 1 - common.jaccard(ref, output[0]) < self.tol
 
     def test_aladin_rigonly(self):
-        size = common.random_float(self.length / 8, self.length * 7 / 8)
-        ref = common.create_circle(self.length, r=size // 2)
-        flo = common.create_circle(self.length, r=size // 2, c=self.length // 2 + 16)
-        output = reg.aladin(ref, flo, pad=0, rigOnly=True, verbose=True)
+        ref = common.create_circle(self.length, r=self.size // 2)
+        flo = common.create_circle(
+            self.length, r=self.size // 2, c=self.length // 2 + 16
+        )
+        output = reg.aladin(ref, flo, pad=0, rigOnly=True, verbose=self.verbose)
         assert 1 - common.jaccard(ref, output[0]) < self.tol
 
     def test_aladin_inaff(self):
-        size = common.random_float(self.length / 8, self.length * 7 / 8)
-        ref = common.create_circle(self.length, r=size // 2)
-        flo = common.create_circle(self.length, r=size // 2, c=self.length // 2 + 16)
+        ref = common.create_circle(self.length, r=self.size // 2)
+        flo = common.create_circle(
+            self.length, r=self.size // 2, c=self.length // 2 + 16
+        )
         _, aff = reg.aladin(ref, flo, verbose=True)
-        output = reg.aladin(ref, flo, pad=0, inaff=aff, verbose=True)
+        output = reg.aladin(ref, flo, pad=0, inaff=aff, verbose=self.verbose)
         assert 1 - common.jaccard(ref, output[0]) < self.tol
 
     def test_aladin_rmask(self):
-        size = common.random_float(self.length / 8, self.length * 7 / 8)
-        ref = common.create_circle(self.length, r=size // 2)
-        flo = common.create_circle(self.length, r=size // 2, c=self.length // 2 + 16)
-        rmask = common.create_rect(self.length, w=size, h=size)
-        output = reg.aladin(ref, flo, pad=0, rmask=rmask, rigOnly=True, verbose=True)
+        ref = common.create_circle(self.length, r=self.size // 2)
+        flo = common.create_circle(
+            self.length, r=self.size // 2, c=self.length // 2 + 16
+        )
+        rmask = common.create_rect(self.length, w=self.size, h=self.size)
+        output = reg.aladin(
+            ref, flo, pad=0, rmask=rmask, rigOnly=True, verbose=self.verbose
+        )
         assert 1 - common.jaccard(ref, output[0]) < self.tol
 
     def test_aladin_fmask(self):
-        size = common.random_float(self.length / 8, self.length * 7 / 8)
-        ref = common.create_circle(self.length, r=size // 2)
-        flo = common.create_circle(self.length, r=size // 2, c=self.length // 2 + 16)
-        fmask = common.create_rect(self.length, c=self.length // 2 + 16, w=size, h=size)
-        output = reg.aladin(ref, flo, pad=0, fmask=fmask, rigOnly=True, verbose=True)
+        ref = common.create_circle(self.length, r=self.size // 2)
+        flo = common.create_circle(
+            self.length, r=self.size // 2, c=self.length // 2 + 16
+        )
+        fmask = common.create_rect(
+            self.length, c=self.length // 2 + 16, w=self.size, h=self.size
+        )
+        output = reg.aladin(
+            ref, flo, pad=0, fmask=fmask, rigOnly=True, verbose=self.verbose
+        )
         assert 1 - common.jaccard(ref, output[0]) < self.tol
 
     def test_f3d_nmi(self):
-        size = common.random_float(self.length / 8, self.length * 7 / 8)
-        ref = common.create_rect(self.length, w=size, h=size)
-
-        flo = transform.swirl(
-            ref,
-            center=(self.length // 2, self.length // 2),
-            strength=10,
-            radius=size,
-        )
-
-        output = reg.f3d(ref, flo, pad=0, nmi=True, rbn=2, fbn=2, verbose=True)
-
+        ref = common.create_rect(self.length, w=self.size, h=self.size)
+        flo = common.apply_swirl(ref, self.length // 2, self.non_linearity, self.size)
+        output = reg.f3d(ref, flo, pad=0, nmi=True, rbn=2, fbn=2, verbose=self.verbose)
         assert 1 - common.jaccard(ref, output[0]) < 1
 
     def test_f3d_lncc(self):
-        size = common.random_float(self.length / 8, self.length * 7 / 8)
-        ref = common.create_rect(self.length, w=size, h=size)
-        flo = transform.swirl(
-            ref,
-            center=(self.length // 2, self.length // 2),
-            strength=self.non_linearity,
-            radius=size,
-        )
-        output = reg.f3d(ref, flo, pad=0, lncc=3, verbose=True)
-
-        assert 1 - common.jaccard(ref, output[0]) < 1
+        ref = common.create_rect(self.length, w=self.size, h=self.size)
+        flo = common.apply_swirl(ref, self.length // 2, self.non_linearity, self.size)
+        output = reg.f3d(ref, flo, pad=0, lncc=3, verbose=self.verbose)
+        assert 1 - common.jaccard(ref, output[0]) < self.tol
 
     def test_f3d_ssd(self):
-        size = common.random_float(self.length / 8, self.length * 7 / 8)
-        ref = common.create_rect(self.length, w=size, h=size)
-        flo = transform.swirl(
-            ref,
-            center=(self.length // 2, self.length // 2),
-            strength=self.non_linearity,
-            radius=size,
-        )
-        output = reg.f3d(ref, flo, pad=0, ssd=True, verbose=True)
-
-        assert 1 - common.jaccard(ref, output[0]) < 1
+        ref = common.create_rect(self.length, w=self.size, h=self.size)
+        flo = common.apply_swirl(ref, self.length // 2, self.non_linearity, self.size)
+        output = reg.f3d(ref, flo, pad=0, ssd=True, verbose=self.verbose)
+        assert 1 - common.jaccard(ref, output[0]) < self.tol
 
     def test_f3d_kld(self):
-        size = common.random_float(self.length / 8, self.length * 7 / 8)
-        ref = common.create_rect(self.length, w=size, h=size)
-        flo = transform.swirl(
-            ref,
-            center=(self.length // 2, self.length // 2),
-            strength=self.non_linearity,
-            radius=size,
-        )
-        output = reg.f3d(ref, flo, pad=0, kld=True, verbose=True)
-
-        assert 1 - common.jaccard(ref, output[0]) < 1
+        ref = common.create_rect(self.length, w=self.size, h=self.size)
+        flo = common.apply_swirl(ref, self.length // 2, self.non_linearity, self.size)
+        output = reg.f3d(ref, flo, pad=0, kld=True, verbose=self.verbose)
+        assert 1 - common.jaccard(ref, output[0]) < self.tol
 
     def test_reg_float(self):
         input = common.random_array((self.length, self.length), np.float32)
-        output = reg.tools(input, float=True)
+        output = reg.tools(input, float=True, verbose=self.verbose)
         assert output is not None
 
     def test_reg_down(self):
         input = common.random_array((self.length, self.length), np.float32)
-        output = reg.tools(input, down=True)
+        output = reg.tools(input, down=True, verbose=self.verbose)
         assert output.shape == tuple(x // 2 for x in input.shape)
 
     def test_reg_smoS_scalar_input(self):
         input = common.random_array((self.length, self.length), np.float32)
-        output = reg.tools(input, smoS=common.random_float())
+        output = reg.tools(input, smoS=common.random_float(), verbose=self.verbose)
         assert output is not None
 
     def test_reg_smoS_tuple_input(self):
         input = common.random_array((self.length, self.length), np.float32)
-        output = reg.tools(input, smoS=common.random_tuple(3))
+        output = reg.tools(input, smoS=common.random_tuple(3), verbose=self.verbose)
         assert output is not None
 
     def test_reg_smoG_scalar_input(self):
         input = common.random_array((self.length, self.length), np.float32)
-        output = reg.tools(input, smoG=common.random_float())
+        output = reg.tools(input, smoG=common.random_float(), verbose=self.verbose)
         assert output is not None
 
     def test_reg_smoG_tuple_input(self):
         input = common.random_array((self.length, self.length), np.float32)
-        output = reg.tools(input, smoG=common.random_tuple(3))
+        output = reg.tools(input, smoG=common.random_tuple(3), verbose=self.verbose)
         assert output is not None
 
     def test_reg_smoL_scalar_input(self):
         input = common.random_array((self.length, self.length), np.float32)
-        output = reg.tools(input, smoL=common.random_float())
+        output = reg.tools(input, smoL=common.random_float(), verbose=self.verbose)
         assert output is not None
 
     def test_reg_smoL_tuple_input(self):
         input = common.random_array((self.length, self.length), np.float32)
-        output = reg.tools(input, smoL=common.random_tuple(3))
+        output = reg.tools(input, smoL=common.random_tuple(3), verbose=self.verbose)
         assert output is not None
