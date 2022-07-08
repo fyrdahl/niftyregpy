c74b44b9d75174020f2ffb1b90605ea8cf289a79
Alexander Fyrdahl <alexander.fyrdahl@gmail.com>
2022-07-08 10:36:49 -0400

    Tests for failed file reads

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,5 +1,6 @@
-import numpy as np
 from niftyregpy import utils
+
+import test_common as common
 
 
 class TestMisc:
@@ -18,3 +19,11 @@ class TestMisc:
     def test_is_function_available_failure(self):
         output = utils.is_function_available("reg_aladin", "default")
         assert output is False
+
+    def test_read_nifti_fail(self):
+        retval = utils.read_nifti(f"{common.random_float()}_test.txt")
+        assert retval is None
+
+    def test_read_txt_fail(self):
+        retval = utils.read_txt(f"{common.random_float()}_test.txt")
+        assert retval is None
