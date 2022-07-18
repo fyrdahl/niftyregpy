29e819500b0c16dc8b5d3a32f1d4e88486477139
Alexander Fyrdahl <alexander.fyrdahl@gmail.com>
2022-07-17 21:06:14 -0400

    Roll back to old setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,36 @@
 from setuptools import setup
 
-setup()
+if __name__ == "__main__":
+
+    REQUIRED_PACKAGES = ["numpy", "nibabel", "tqdm"]
+
+    setup(
+        name="niftyregpy",
+        version="0.0.2",
+        author="fyrdahl",
+        author_email="fyrdahl@med.umich.edu",
+        description="Python interface for NiftyReg",
+        packages=[
+            "niftyregpy",
+            "niftyregpy.average",
+            "niftyregpy.apps",
+            "niftyregpy.reg",
+            "niftyregpy.tools",
+            "niftyregpy.transform",
+            "niftyregpy.utils",
+        ],
+        url="https://github.com/fyrdahl/niftyregpy",
+        classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Scientific/Engineering :: Medical Science Apps",
+        ],
+        install_requires=REQUIRED_PACKAGES,
+        license="MIT",
+        project_urls={"Source": "https://github.com/fyrdahl/niftyregpy"},
+        package_dir={"": "src"},
+    )
