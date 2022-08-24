2694a401f7f8ae7f44eed0907340694514626a4e
Alexander Fyrdahl <alexander.fyrdahl@gmail.com>
2022-08-24 10:45:51 +0200

    docs [no ci]

--- src/niftyregpy/tools/tools.py
+++ src/niftyregpy/tools/tools.py
@@ -46,7 +46,7 @@ def down(input, output=None, verbose=False):
     The input image is downsampled 2 times.
 
     Args:
-        input (array): Input array to be converted.
+        input (array): Input array to be downsampled.
         output (string): Specify output file (optional).
         verbose (bool): Verbose output (default = False).
 
@@ -79,7 +79,7 @@ def smoS(input, output=None, sx=0.0, sy=0.0, sz=0.0, verbose=False):
     The input image is smoothed using a cubic b-spline kernel.
 
     Args:
-        input (array): Input array to be converted.
+        input (array): Input array to be smoothed.
         sx (float): Smoothing in x.
         sy (float): Smoothing in y.
         sz (float): Smoothing in z.
@@ -115,7 +115,7 @@ def smoG(input, output=None, sx=0.0, sy=0.0, sz=0.0, verbose=False):
     The input image is smoothed using a Gaussian kernel.
 
     Args:
-        input (array): Input array to be converted.
+        input (array): Input array to be smoothed.
         sx (float): Smoothing in x.
         sy (float): Smoothing in y.
         sz (float): Smoothing in z.
@@ -152,7 +152,7 @@ def smoL(input, output=None, sx=0.0, sy=0.0, sz=0.0, verbose=False):
     The input label image is smoothed using a Gaussian kernel.
 
     Args:
-        input (array): Input array to be converted.
+        input (array): Input array to be smoothed.
         sx (float): Smoothing in x.
         sy (float): Smoothing in y.
         sz (float): Smoothing in z.
@@ -189,8 +189,8 @@ def add(input, x, output=None, verbose=False):
     This image (or value) is added to the input.
 
     Args:
-        input (array): Input array to be converted.
-        x (array/float): Image or value to be added to the input.
+        input (array): Input array.
+        x (array/float): Image or value to be added to the input array.
         output (string): Specify output file (optional).
         verbose (bool): Verbose output (default = False).
 
@@ -229,8 +229,8 @@ def sub(input, x, output=None, verbose=False):
     This image (or value) is subtracted from the input
 
     Args:
-        input (array): Input array to be converted.
-        x (array/float): Image or value to be subtracted from the input.
+        input (array): Input array.
+        x (array/float): Image or value to be subtracted from the input array.
         output (string): Specify output file (optional).
         verbose (bool): Verbose output (default = False).
 
@@ -269,8 +269,8 @@ def mul(input, x, output=None, verbose=False):
     This image (or value) is multiplied with the input
 
     Args:
-        input (array): Input array to be converted.
-        x (array/float): Image or value to be multiplied with the input.
+        input (array): Input array.
+        x (array/float): Image or value to be multiplied with the input array.
         output (string): Specify output file (optional).
         verbose (bool): Verbose output (default = False).
 
@@ -307,9 +307,10 @@ def div(input, x, output=None, verbose=False):
 
     """
     This image (or value) is divided to the input
+
     Args:
-        input (array): Input array to be converted.
-        x (array/float): Image or value to be multiplied with the input.
+        input (array): Input array.
+        x (array/float): Image or value to divide the input array by.
         output (string): Specify output file (optional).
         verbose (bool): Verbose output (default = False).
 
@@ -342,10 +343,17 @@ def div(input, x, output=None, verbose=False):
     return None
 
 
-def rms(input, x, output=None, verbose=False):
+def rms(input, input2, output=None, verbose=False):
 
     """
     Compute the mean rms between both images
+
+    Args:
+        input (array): First input array.
+        input2 (array): Second input array.
+        output (string): Specify output file (optional).
+        verbose (bool): Verbose output (default = False).
+
     """
 
     with tmp.TemporaryDirectory() as tmp_folder:
@@ -353,7 +361,7 @@ def rms(input, x, output=None, verbose=False):
         cmd_str = "reg_tools"
 
         write_nifti(path.join(tmp_folder, "input.nii"), input)
-        write_nifti(path.join(tmp_folder, "x.nii"), x)
+        write_nifti(path.join(tmp_folder, "input2.nii"), input2)
 
         cmd_str += " -in " + path.join(tmp_folder, "input.nii")
 
@@ -361,7 +369,7 @@ def rms(input, x, output=None, verbose=False):
             output = path.join(tmp_folder, "output.nii")
 
         cmd_str += f" -out {output}"
-        cmd_str += " -rms " + path.join(tmp_folder, "x.nii")
+        cmd_str += " -rms " + path.join(tmp_folder, "input2.nii")
 
         out = call_niftyreg(cmd_str, verbose, output_stdout=True)
 
@@ -375,6 +383,11 @@ def bin(input, output=None, verbose=False):
 
     """
     Binarize the input image (val!=0?val=1:val=0)
+
+    Args:
+        input (array): Input array to be binarized.
+        output (string): Specify output file (optional).
+        verbose (bool): Verbose output (default = False).
     """
 
     with tmp.TemporaryDirectory() as tmp_folder:
@@ -400,6 +413,11 @@ def thr(input, thr, output=None, verbose=False):
 
     """
     Threshold the input image (val<thr?val=0:val=1)
+
+    Args:
+        input (array): Input array to be thresholded.
+        output (string): Specify output file (optional).
+        verbose (bool): Verbose output (default = False).
     """
 
     with tmp.TemporaryDirectory() as tmp_folder:
@@ -421,18 +439,24 @@ def thr(input, thr, output=None, verbose=False):
         return None
 
 
-def nan(input, x, output=None, verbose=False):
+def nan(input, mask, output=None, verbose=False):
 
     """
     This image is used to mask the input image.
-    Voxels outside of the mask are set to nan
+    Voxels outside of the mask are set to NaN
+
+    Args:
+        input (array): Input array.
+        mask (array): Input mask, values outside mask is set to NaN.
+        output (string): Specify output file (optional).
+        verbose (bool): Verbose output (default = False).
     """
     with tmp.TemporaryDirectory() as tmp_folder:
 
         cmd_str = "reg_tools"
 
         write_nifti(path.join(tmp_folder, "input.nii"), input)
-        write_nifti(path.join(tmp_folder, "x.nii"), x)
+        write_nifti(path.join(tmp_folder, "mask.nii"), mask)
         cmd_str += " -in " + path.join(tmp_folder, "input.nii")
 
         if output is None:
@@ -451,6 +475,11 @@ def iso(input, output=None, verbose=False):
 
     """
     The resulting image is made isotropic
+
+    Args:
+        input (array): Input array to be made isotropic.
+        output (string): Specify output file (optional).
+        verbose (bool): Verbose output (default = False).
     """
 
     with tmp.TemporaryDirectory() as tmp_folder:
@@ -476,6 +505,11 @@ def noscl(input, output=None, verbose=False):
 
     """
     The scl_slope and scl_inter are set to 1 and 0 respectively
+
+    Args:
+        input (array): Input array.
+        output (string): Specify output file (optional).
+        verbose (bool): Verbose output (default = False).
     """
 
     with tmp.TemporaryDirectory() as tmp_folder:
@@ -497,10 +531,18 @@ def noscl(input, output=None, verbose=False):
     return None
 
 
-def chgres(input, output=None, sx=0.0, sy=0.0, sz=0.0, verbose=False):
+def chgres(input, sx=0.0, sy=0.0, sz=0.0, output=None, verbose=False):
 
     """
     Resample the input image to the specified resolution (in mm)
+
+    Args:
+        input (array): Input array to be resampled.
+        sx (float): Resolution in x.
+        sy (float): Resolution in y.
+        sz (float): Resolution in z.
+        output (string): Specify output file (optional).
+        verbose (bool): Verbose output (default = False).
     """
     cmd_str = "reg_tools"
 
@@ -524,10 +566,16 @@ def chgres(input, output=None, sx=0.0, sy=0.0, sz=0.0, verbose=False):
     return None
 
 
-def rmNanInf(input, output=None, x=0.0, verbose=False):
+def rmNanInf(input, x=0.0, output=None, verbose=False):
 
     """
-    Remove the nan and inf from the input image and replace them by the specified value
+    Remove NaN and Inf values from the input image and replace with specified value
+
+    Args:
+        input (array): Input array.
+        x (float): Value that should be used to replace NaN and Inf (default = 0.0).
+        output (string): Specify output file (optional).
+        verbose (bool): Verbose output (default = False).
     """
 
     cmd_str = "reg_tools"
@@ -555,7 +603,12 @@ def rmNanInf(input, output=None, x=0.0, verbose=False):
 def testActiveBlocks(input, output=None, verbose=False):
 
     """
-    Generate an image highlighting the active blocks for reg_aladin (block variance is shown)
+    Generate image showing the active blocks for reg.aladin (block variance is shown)
+
+    Args:
+        input (array): Input array.
+        output (string): Specify output file (optional).
+        verbose (bool): Verbose output (default = False).
     """
 
     cmd_str = "reg_tools"
